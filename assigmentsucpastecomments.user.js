4499d607d9a36ad85e2e9ce7b1dc8f9124f60f6e
Jorge C. S. Cardoso <jorgecardoso@ieee.org>
2020-06-10 16:03:21 +0100

    added copyright

--- assigmentsucpastecomments.user.js
+++ assigmentsucpastecomments.user.js
@@ -2,9 +2,10 @@
 // @name         Assignments UC Paste Comments
 // @namespace    https://jorgecardoso.eu
 // @version      0.1
-// @license     License Type; License Homepage
+// @license      License Type; License Homepage
 // @description  Allows pasting Comments directly on the Assignments page of the InforDocente system of the University of Coimbra. Just copy (Ctrl-C) a table from a spreadsheet file making sure that there is a column with the student number and a column with the comments (the comments column should be the last column). Then open the assignment page and press Ctrl-V; a button will appear: click on it and wait for all comments to be inserted.
 // @author       jorgecardoso
+// @copyright    2020, Jorge C. S. Cardoso (https://jorgecardoso.eu)
 // @match        https://infordocente.uc.pt/nonio/ensino/detalhesSubmissaoTrabalhos.do*
 // @match        https://infordocente.uc.pt/nonio/ensino/detalhesSubmissaoAluno.do*
 // @match        https://infordocente.uc.pt/nonio/ensino/adicionarAlterarApreciacao.do*
